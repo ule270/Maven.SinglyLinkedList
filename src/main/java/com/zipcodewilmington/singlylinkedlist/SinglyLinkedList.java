8ce76b3501b0023218e143a705c5a334846416eb
ule270 <u.le270@gmail.com>
2023-04-02 18:51:37 -0400

    LinkedList set up

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -4,4 +4,28 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+    private Node head;
+    private Node next;
+    private int data;
+    static class Node{
+        Node head;
+        Node next;
+        int data;
+    }
+    public SinglyLinkedList(Node next, int data){
+        this.node = next;
+        this.data = data;
+    }
+//    add -- add an element to the list
+    public void addElement(){
+
+    }
+//    remove -- remove an element (specified by numeric index) from the list
+//    contains -- returns true if the element is in the list, false otherwise
+//    find -- returns the element's index if it is in the list, -1 otherwise
+//    size -- returns the current size of the list
+//    get -- returns the element at the specified index
+//    copy -- returns a new linked list containing the same values (look up deep versus shallow copy)
+//    sort -- sorts the list using your algorithm of choice. You must perform the sorting yourself (no fair using someone else's library)
+
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,43 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    @Test
+    public void testAdd(){
+
+    }
+    @Test
+    public void testRemove(){
+
+    }
+    @Test
+    public void testContains(){
+
+    }
+    @Test
+    public void testFind(){
+
+    }
+
+    @Test
+    public void testSize(){
+
+    }
+
+    @Test
+    public void testGet(){
+
+    }
+    @Test
+    public void testCopy(){
+
+    }
+    @Test
+    public void testSort(){
+
+    }
 }
